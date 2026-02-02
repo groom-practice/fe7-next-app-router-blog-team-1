45bfbfd2c0e4bd767f19e7823f47e607472e65c2
Leemainsw <yebom6476@gmail.com>
2026-02-02 10:02:54 +0900

    refactor: tailwind css 사용을 위해 global.css, layout.js revert

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import style from "./layout.module.css";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>
+      <body className='p-6'>
         <header className={style.header}>
           <Link href="/">홈</Link> <Link href="/write">글 작성</Link>
         </header>
